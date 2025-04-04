a347ef6e2a49ca1ed79a765ab30468b59eb1bcf6
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 22:17:10 +0500

    Update loop6.cpp

--- loop6.cpp
+++ loop6.cpp
@@ -1,1 +1,8 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    char c;
+    cout << "enter a character: ";
+    cin >> c;
+    cout << "output: " << (char)(islower(c) ? toupper(c) : tolower(c)) << endl;
+}
