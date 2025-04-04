36b2202290fab1697420607066b2dcdb73e3d74a
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 23:20:24 +0500

    Update function4.cpp

--- function4.cpp
+++ function4.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    int a = 0, b = 1, c;
+    while (a <= 10000) {
+        if (a % 5 == 0) cout << setw(5) << setfill('0') << a << " ";
+        c = a + b, a = b, b = c;
+    }
+}
