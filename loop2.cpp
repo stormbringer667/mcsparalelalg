5abd1f78fe8dca561a447b01bea68d60516f3cea
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 21:50:01 +0500

    Update loop2.cpp

--- loop2.cpp
+++ loop2.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    float num;
+    cout << "enter decimal number: ";
+    cin >> num;
+    cout << "binary: 0.";
+    while (num > 0.005) {
+        num *= 2;
+        cout << (int)num;
+        num -= (int)num;
+    }
+    cout << endl;
+}
