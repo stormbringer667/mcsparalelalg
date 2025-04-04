c2e4255f910c47916b489af06af911fcf6019049
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 22:47:40 +0500

    Update loop13.cpp

--- loop13.cpp
+++ loop13.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "enter an int: ";
+    cin >> n;
+    cout << "reversed: ";
+    do cout << n % 10, n /= 10; while (n);
+    cout << endl;
+}
