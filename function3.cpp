b5a34776cb9d98cd0dd49184943de9f933a85f71
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 23:19:43 +0500

    Update function3.cpp

--- function3.cpp
+++ function3.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+using namespace std;
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) if (n % i == 0) return false;
+    return true;
+}
+int main() {
+    for (int i = 2; i <= 1000; i++) if (isPrime(i)) cout << i << " ";
+}
