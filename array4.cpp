4f0449388576a215dcf516cede9d9e99f5d1f005
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 23:39:30 +0500

    Update array4.cpp

--- array4.cpp
+++ array4.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+void printMatrix(int* arr, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) cout << arr[i * cols + j] << " ";
+        cout << endl;
+    }
+}
+int main() {
+    int r = 3, c = 4, arr[] = { 1,2,3,4,5,6,7,8,9,10,11,12 }, transposed[12];
+    cout << "orig matrix:\n"; printMatrix(arr, r, c);
+    for (int i = 0; i < r; i++) for (int j = 0; j < c; j++) transposed[j * r + i] = arr[i * c + j];
+    cout << "transported matrix:\n"; printMatrix(transposed, c, r);
+}
