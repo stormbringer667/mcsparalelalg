5b3ee8a1f02f7283a85423820452488316311d8a
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 22:44:59 +0500

    Update loop11.cpp

--- loop11.cpp
+++ loop11.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "enter number : ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cout << string(2 * i + 1, 'A' + i) << endl;
+}
