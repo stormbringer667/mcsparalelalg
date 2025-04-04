9dd7cfe5cafc531ca40805a957405bc0f9518a53
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 21:49:38 +0500

    Update loop1.cpp

--- loop1.cpp
+++ loop1.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool isArmstrong(int num) {
+    int sum = 0, temp = num, digits = log10(num) + 1;
+    while (temp) sum += pow(temp % 10, digits), temp /= 10;
+    return sum == num;
+}
+int main() {
+    for (int i = 1; i < 100000; i++)
+        if (isArmstrong(i)) cout << i << " ";
+    cout << endl;
+}
