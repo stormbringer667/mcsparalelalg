519277a568001dbe68f5cdb8ea8b8dea1882283a
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 22:54:23 +0500

    Update loop15.cpp

--- loop15.cpp
+++ loop15.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+bool sum25(int num) {
+    int sum = 0;
+    while (num) sum += num % 10, num /= 10;
+    return sum == 25;
+}
+int main() {
+    for (int i = 10000000, count = 0; i < 100000000; i++) {
+        if (sum25(i)) cout << i << (++count % 10 ? " " : "\n");
+    }
+    cout << endl;
+}
