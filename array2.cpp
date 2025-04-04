7c75d2061aa1a20bc7e0ebe60293bcd9ef7d5354
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 23:35:12 +0500

    Update array2.cpp

--- array2.cpp
+++ array2.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int y, m, d, days[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    cout << "enter year, month, day: ";
+    cin >> y >> m >> d;
+    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) days[1] = 29; 
+    int dayOfYear = d;
+    for (int i = 0; i < m - 1; i++) dayOfYear += days[i];
+    cout << "day of the year: " << dayOfYear << endl;
+}
