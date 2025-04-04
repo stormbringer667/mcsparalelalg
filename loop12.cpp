493aa9a37448387b17d0a0b437b832dfffe273cc
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 22:45:48 +0500

    Update loop12.cpp

--- loop12.cpp
+++ loop12.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    char ch = 'A';
+    cout << "enter number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++, cout << endl)
+        for (int j = 0; j < 2 * i - 1; j++) cout << ch++;
+}
