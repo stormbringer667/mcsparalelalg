9af748049bac6751b3053ce7500b721f9f377718
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 21:51:49 +0500

    Update loop3.cpp

--- loop3.cpp
+++ loop3.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "enter number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cout << string(i, 'A') << " " << string(i, 'B') << endl;
+}
