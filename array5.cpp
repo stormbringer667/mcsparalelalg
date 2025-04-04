a6201884ff89ec0390c38adf577fc81076037979
stormbringer667 <157335208+stormbringer667@users.noreply.github.com>
2025-04-04 23:40:40 +0500

    Update array5.cpp

--- array5.cpp
+++ array5.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = { 4, 2, 9, 1, 7 }, n = 5, minIdx = 0, maxIdx = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < arr[minIdx]) minIdx = i;
+        if (arr[i] > arr[maxIdx]) maxIdx = i;
+    }
+    cout << "before swap: "; for (int x : arr) cout << x << " ";
+    swap(arr[minIdx], arr[maxIdx]);
+    cout << "\nafter swap: "; for (int x : arr) cout << x << " ";
+}
